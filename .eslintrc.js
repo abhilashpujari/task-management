6094535b6b7b0759d568afad6edeaf34732bdd81
abhilash <abhilashpujari11@gmail.com>
2020-11-16 13:41:19 +0530

    Added bootstrap-vue and tailwind css

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,60 @@
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  extends: ['plugin:vue/recommended', 'airbnb-base'],
-  plugins: [
-    'vue'
-  ],
-  settings: {
-    'import/resolver': {
-      webpack: {
-        config: 'build/webpack.base.conf.js'
-      }
+    root: true,
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    env: {
+        browser: true,
+    },
+    extends: ['plugin:vue/recommended', 'airbnb-base'],
+    plugins: [
+        'vue'
+    ],
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: 'build/webpack.base.conf.js'
+            }
+        }
+    },
+    // add your custom rules here
+    rules: {
+        'camelcase': ['off'],
+        'comma-dangle': ['off'],
+        'consistent-return': ['off'],
+        'class-methods-use-this': ['off'],
+        'dot-notation': ['off'],
+        'default-case': ['off'],
+        'eol-last': ['off'],
+        // don't require .vue extension when importing
+        'import/extensions': ['off'],
+        'import/first': ['off'],
+        'import/no-unresolved': ['off'],
+        'import/no-extraneous-dependencies': ['off'],
+        'import/no-duplicates': ['off'],
+        indent: ['off'],
+        'linebreak-style': ['off'],
+        'func-names': ['off'],
+        'max-len': ['off'],
+        'no-new': ['off'],
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-prototype-builtins': ['off'],
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-param-reassign': ['off'],
+        'no-underscore-dangle': ['off'],
+        'no-undef': ['off'],
+        'no-unused-vars': ['off'],
+        'object-curly-spacing': ['off'],
+        'prefer-const': ['off'],
+        'prefer-destructuring': ['off'],
+        'semi': ['off'],
+        'vue/html-indent': ['off'],
+        'vue/html-closing-bracket-spacing': ['off'],
+        'vue/html-closing-bracket-newline': ['off'],
+        'vue/multiline-html-element-content-newline': ['off'],
+        'vue/no-lone-template': ['off'],
+        'vue/custom-event-name-casing': ['off'],
+        'vue/max-attributes-per-line': ['off'],
+        'vue/require-default-prop': ['off']
     }
-  },
-  // add your custom rules here
-  rules: {
-    'camelcase': ['off'],
-    'comma-dangle': ['off'],
-    'consistent-return': ['off'],
-    'class-methods-use-this': ['off'],
-    'dot-notation': ['off'],
-    'default-case': ['off'],
-    'eol-last': ['off'],
-    // don't require .vue extension when importing
-    'import/extensions': ['off'],
-    'import/no-unresolved': ['off'],
-    'import/no-extraneous-dependencies': ['off'],
-    'import/no-duplicates': ['off'],
-    indent: ['off'],
-    'linebreak-style': ['off'],
-    'func-names': ['off'],
-    'max-len': ['off'],
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-prototype-builtins': ['off'],
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-param-reassign': ['off'],
-    'no-underscore-dangle': ['off'],
-    'no-undef': ['off'],
-    'no-unused-vars': ['off'],
-    'object-curly-spacing': ['off'],
-    'prefer-const': ['off'],
-    'prefer-destructuring': ['off'],
-    'semi': ['off'],
-    'vue/html-indent': ['off'],
-    'vue/html-closing-bracket-spacing': ['off'],
-    'vue/html-closing-bracket-newline': ['off'],
-    'vue/multiline-html-element-content-newline': ['off'],
-    'vue/no-lone-template': ['off'],
-    'vue/custom-event-name-casing': ['off'],
-    'vue/max-attributes-per-line': ['off'],
-    'vue/require-default-prop': ['off']
-  }
-}
+};
--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@/assets/css/tailwind.css';
+
+library.add(faSearch);
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 new Vue({
   el: '#app',
   router,
