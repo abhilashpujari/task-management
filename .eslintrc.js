84c4d32836e89cc2bd299d3568f6ed52089d02aa
abhilash <abhilashpujari11@gmail.com>
2020-11-16 11:46:12 +0530

    eslint rules update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,3 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parserOptions: {
@@ -8,14 +6,10 @@ module.exports = {
   env: {
     browser: true,
   },
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential', 'airbnb-base'],
-  // required to lint *.vue files
+  extends: ['plugin:vue/recommended', 'airbnb-base'],
   plugins: [
     'vue'
   ],
-  // check if imports actually resolve
   settings: {
     'import/resolver': {
       webpack: {
@@ -25,26 +19,40 @@ module.exports = {
   },
   // add your custom rules here
   rules: {
+    'camelcase': ['off'],
+    'comma-dangle': ['off'],
+    'consistent-return': ['off'],
+    'class-methods-use-this': ['off'],
+    'dot-notation': ['off'],
+    'default-case': ['off'],
+    'eol-last': ['off'],
     // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      js: 'never',
-      vue: 'never'
-    }],
-    // disallow reassignment of function parameters
-    // disallow parameter object manipulation except for specific exclusions
-    'no-param-reassign': ['error', {
-      props: true,
-      ignorePropertyModificationsFor: [
-        'state', // for vuex state
-        'acc', // for reduce accumulators
-        'e' // for e.returnvalue
-      ]
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      optionalDependencies: ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'import/extensions': ['off'],
+    'import/no-unresolved': ['off'],
+    'import/no-extraneous-dependencies': ['off'],
+    'import/no-duplicates': ['off'],
+    indent: ['off'],
+    'linebreak-style': ['off'],
+    'func-names': ['off'],
+    'max-len': ['off'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-prototype-builtins': ['off'],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': ['off'],
+    'no-underscore-dangle': ['off'],
+    'no-undef': ['off'],
+    'no-unused-vars': ['off'],
+    'object-curly-spacing': ['off'],
+    'prefer-const': ['off'],
+    'prefer-destructuring': ['off'],
+    'semi': ['off'],
+    'vue/html-indent': ['off'],
+    'vue/html-closing-bracket-spacing': ['off'],
+    'vue/html-closing-bracket-newline': ['off'],
+    'vue/multiline-html-element-content-newline': ['off'],
+    'vue/no-lone-template': ['off'],
+    'vue/custom-event-name-casing': ['off'],
+    'vue/max-attributes-per-line': ['off'],
+    'vue/require-default-prop': ['off']
   }
 }
